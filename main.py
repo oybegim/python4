e730ac5597ab09258b6f49edd83708eb4fc102cb
oybegim <sultonovaoybegim@main.com>
2022-10-31 22:52:59 +0300

    birnima

---
+++ main.py
@@ -0,0 +1,25 @@
+import time
+a = 0
+while a< 1:
+    a += 0.3
+    time.sleep(1)
+    print(a)
+def sanash():
+    b = 0
+    while b < 60:
+        b +=1
+        print(b)
+sanash()
+
+
+c = 0
+while c< 3:
+    c += 0.3
+    time.sleep(3)
+    print(c)
+def sanash1():
+    d = 0
+    while d<60:
+        d += 1
+        print(d) 
+sanash1()
---
+++ main65.py
@@ -0,0 +1,5 @@
+import time
+def func1():
+    for x in range(0, 30):
+        time.sleep(0.2)
+        print("Sulonova")
